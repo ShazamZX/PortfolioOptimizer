98287061c81cf3f69e85b6e6b5f8f63f24547299
ShazamZX <sancharbanerjee15@gmail.com>
2022-03-08 22:03:00 +0530

    name change

--- optimustock/__init__.py
+++ optimustock/__init__.py
@@ -1,3 +1,3 @@
 import imp
-from optimustock.PortfolioOptimizer import PortfolioOptimizer
+from optimustock.PortfolioOptimizer import MarkowitzPortfolioOptimizer
 from optimustock.StockDataLoader import StockDataLoader
